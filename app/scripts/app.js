6327f63657d704be50182a2e9d1c2499349a76b0
Rob Thijssen <rthijssen@gmail.com>
2015-07-29 11:02:54 +0100

    yo scaffold

---
+++ app/scripts/app.js
@@ -0,0 +1,35 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name parkattackgithubioApp
+ * @description
+ * # parkattackgithubioApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('parkattackgithubioApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl',
+        controllerAs: 'about'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name parkattackgithubioApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the parkattackgithubioApp
+ */
+angular.module('parkattackgithubioApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
