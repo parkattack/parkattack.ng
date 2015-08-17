d187c271f022a2b356eb32ee357018f21de79d1c
Rob Thijssen <rthijssen@gmail.com>
2015-08-17 10:57:24 +0100

    logic correction

--- publish.sh
+++ publish.sh
@@ -3,10 +3,10 @@
 git clone "https://${GH_TOKEN}@${GH_REF}" pages_repo
 rm -rf pages_repo/*
 cp -r dist/* pages_repo/
-if [ -n "$GH_PAGES_CNAME" ]; then
+if [[ -n "$GH_PAGES_CNAME" ]]; then
   echo $GH_PAGES_CNAME> pages_repo/CNAME
 fi
-if ! git --git-dir=./pages_repo/.git --work-tree=./pages_repo status --porcelain; then
+if [[ `git --git-dir=./pages_repo/.git --work-tree=./pages_repo status --porcelain` ]]; then
   git --git-dir=./pages_repo/.git --work-tree=./pages_repo config user.name "${GIT_NAME}"
   git --git-dir=./pages_repo/.git --work-tree=./pages_repo config user.email "${GIT_EMAIL}"
   git --git-dir=./pages_repo/.git --work-tree=./pages_repo add . -A
